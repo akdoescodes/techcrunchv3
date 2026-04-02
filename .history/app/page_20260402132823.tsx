57c47deb910157a06f2df981ee669722e65e2d83
Arjun Kale <arjungease@gmail.com>
2026-04-02 13:33:18 +0530

    final 2026 commit

--- .history/app/page_20260402132823.tsx
+++ .history/app/page_20260402132823.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "@/components/navbar";
 import TechEventCard from "@/components/tech-event-card";
 import TechEditForm from "@/components/tech-edit-form";
